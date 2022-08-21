c74ac63c33ad77c67ce613b4a7323cf2a8c1e0b5
Maor <m@oritzio.com>
2022-08-21 09:19:38 -0400

    Add 12-Hour format option

--- index.js
+++ index.js
@@ -47,7 +47,13 @@ function ordinalSuffix(num) {
 }
 
 // the convert function
-const convertToWords = (rule) => {
+const convertToWords = (rule, timeformat) => {
+
+  // check if function is getting a string
+  if ( typeof rule != "string") {
+    console.log('Expecting a string')
+    return;
+  }
 
   // remove the RRULE: from the string
   rule = rule.replace("RRULE:", "");
@@ -66,30 +72,56 @@ const convertToWords = (rule) => {
   let freq = ruleObject["FREQ"];
   let byday = ruleObject["BYDAY"];
   let bymonthday = ruleObject["BYMONTHDAY"];
+  let byhour = ruleObject["BYHOUR"];
+  let byminute = ruleObject["BYMINUTE"];
   let count = ruleObject["COUNT"];
   let interval = ruleObject["INTERVAL"];
+
+  // initiate output
   let output = "";
 
   // format
+  // daily
   if ( freq === "DAILY" ) {
 
-      if ( ruleObject["BYMINUTE"] ) {
-        output = `Every day at ${ruleObject["BYHOUR"]}:${ruleObject["BYMINUTE"]}`;
-      } else {
-        output = `Every day at ${ruleObject["BYHOUR"]}:00`;
-      }
+    if ( byminute ) {
+      output = `Every day at ${byhour}:${byminute}`;
+    } else {
+      output = `Every day at ${byhour}:00`;
+    }
 
-      if ( count ) {
-        if ( count == "1" ) {
-          output += ` for 1 time`;
-        } else {
-          output += ` for ${count} times`;
-        }
+    if ( timeformat == "12HM") {
+      let ampm = "";
+      if ( byhour >= 12 && byhour < 24 ) {
+        ampm = "PM";
+      } else {
+        ampm = "AM";
       }
+      output = `Every day at ${byhour > 12 ? byhour - 12 : byhour}:${byminute} ${ampm}`;
+    }
+
+    if ( timeformat == "12H") {
+      let ampm = "";
+      if ( byhour >= 12 && byhour < 24 ) {
+        ampm = "PM";
+      } else {
+        ampm = "AM";
+      }
+      output = `Every day at ${byhour > 12 ? byhour - 12 : byhour}${ampm}`;
+    }
+
+    if ( count ) {
+      if ( count == "1" ) {
+        output += ` for 1 time`;
+      } else {
+        output += ` for ${count} times`;
+      }
+    }
 
     return output;
   }
 
+  // weekly
   if ( freq === "WEEKLY" ) {
 
     if ( interval && parseInt(interval) > 1 ) {
@@ -118,6 +150,7 @@ const convertToWords = (rule) => {
     return output;
    }
 
+   // monthly
    if ( freq === "MONTHLY" ) {
 
      if ( interval && parseInt(interval) > 1 ) {
--- readme.md
+++ readme.md
@@ -12,24 +12,52 @@ npm install rruleconverter
 ```js
 // import
 import rruleToWords from "rruleconverter";
+or
+const rruleToWords = require('rruleconverter')
 
 // call the function and pass the rrule as a string
 rruleToWords("RRULE:FREQ=WEEKLY;BYDAY=TH")
 
 // output for this example
-Every week on Thursday
+// Every week on Thursday
 
 ```
 
 
 ## Output Examples
 
-```
+```js
 rruleToWords("RRULE:FREQ=DAILY;BYHOUR=11")  // Every day at 11:00
 rruleToWords("RRULE:FREQ=DAILY;BYHOUR=13;BYMINUTE=30;COUNT=5")  // Every day at 13:30 for 5 times
 rruleToWords("RRULE:FREQ=WEEKLY;BYDAY=30;INTERVAL=2;COUNT=3")  // Every 2 weeks on Monday for 3 times
 rruleToWords("RRULE:FREQ=MONTHLY;BYMONTHDAY=2")  // Every month on the 2nd
 rruleToWords("RRULE:FREQ=MONTHLY;BYMONTHDAY=23;COUNT=6")  // Every month on 23rd for 6 times
+```
+
+## Options
+
+```js
+rruleToWords(rrule, timeformat)
+
+// arguments
+1. rrule - "RRULE:FREQ=DAILY;BYHOUR=11"
+2. timeformat - "12HM", "12M" (optional)
+```
+
+The second argument is optional and for defining the time format.
+If not specified it will show the time as 24-Hour format
+
+```js
+rruleToWords("RRULE:FREQ=DAILY;BYHOUR=15")  // Every day at 15:00
+```
+#### Customize the time format to 12-Hour format
+
+
+```js
+rruleToWords("RRULE:FREQ=DAILY;BYHOUR=11", "12HM")  // Every day at 11:00 AM
+rruleToWords("RRULE:FREQ=DAILY;BYHOUR=18;BYMINUTE=30", "12HM")  // Every day at 6:30 PM
+rruleToWords("RRULE:FREQ=DAILY;BYHOUR=20;BYMINUTE=30", "12H")  // Every day at 8PM
+rruleToWords("RRULE:FREQ=DAILY;BYHOUR=13;BYMINUTE=30;COUNT=5", "12H")  // Every day at 1PM for 5 times
 ```
 
 
